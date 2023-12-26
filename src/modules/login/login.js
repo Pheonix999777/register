1d73ab0f8d83eee30d97d0fa05811db037e40f98
Pheonix999777 <Pheonix4777@gmail.com>
2023-12-26 15:42:40 +0500

    first

---
+++ src/modules/login/login.js
@@ -0,0 +1,30 @@
+
+let elform = document.querySelector(".dark__form")
+
+
+let userdata = localStorage.getItem("userdata")
+
+// if(userdata){
+//     location.pathname = "/src/modules/home/index.html"
+// }
+
+let elinp = document.querySelector(".dark__inp")
+let elinpPassword = document.querySelector(".dark__password")
+if(elform){
+    elform.addEventListener("submit", (e)=>{
+        e.preventDefault();
+        let inp = elinp.value.trim()  
+        let inpPassword = elinpPassword.value.trim() 
+        if(localStorage.getItem("userdata").includes(inp && inpPassword)){
+            location.pathname = "/src/modules/home/index.html"
+        }else{
+            alert("Username yoki parol xato")
+        }
+
+    })
+}
+
+let darkbtn = document.querySelector(".dark__link")
+darkbtn.addEventListener("click", ()=>{
+    location.pathname = "/src/modules/register/index.html"
+})
